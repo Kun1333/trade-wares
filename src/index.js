6224188c0ec49a81f2739e93807c438c3edb6283
Kun1333 <kun1333@hotmail.com>
2018-03-26 20:54:47 +0300

    index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Route} from 'react-router';
+import {HashRouter} from 'react-router-dom'
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import Immutable from 'immutable';
